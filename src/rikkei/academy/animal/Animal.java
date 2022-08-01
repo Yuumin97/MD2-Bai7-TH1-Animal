534e374c2f81773258e8705488ce4afc86c4da09
Yuumin97 <fiesportlmtc@gmail.com>
2022-08-01 23:08:02 +0700

    js

---
+++ src/rikkei/academy/animal/Animal.java
@@ -0,0 +1,4 @@
+package rikkei.academy.animal;
+public abstract class Animal {
+    public abstract String makeSound();
+}
---
+++ src/rikkei/academy/animal/Tiger.java
@@ -0,0 +1,10 @@
+package rikkei.academy.animals;
+
+import rikkei.academy.animal.Animal;
+
+public class Tiger extends Animal {
+    @Override
+    public String makeSound() {
+        return "Tiger: roarrrrr!";
+    }
+}
